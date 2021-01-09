d2775a6c97d38bb19d67d85ed981e4f92609595e
SyFlores <75463000+SyFlores@users.noreply.github.com>
2021-01-09 07:55:59 -0800

    Adding main PyBank python file (#5)
    * Adding main PyBank python file
    
    This will serve as the primary script to run for the PyBank problem. At the time of publishing, it is only able to read in the file from a local directory that has not yet been synced to this repository. This will be changed in the future and the file path will be updated to reflect that.
    
    * Add files via upload
    
    This will serve as the primary script to run for the PyPoll problem. At the time of publishing, it is only able to read in the file from a local directory that has not yet been synced to this repository. This will be changed in the future and the file path will be updated to reflect that.

---
+++ PyPoll/main.py
@@ -0,0 +1,22 @@
+import os
+import csv
+
+
+
+# Update this when you get your local repo syncing
+# Doing this here to avoid accidentally deletin anything
+csvpath = os.path.join('..', 'PyPoll', 'election_data.csv')
+
+with open(csvpath) as csvfile:
+
+    csvreader = csv.reader(csvfile, delimiter=',')
+
+    print(csvreader)
+
+    # Read the header row first (skip this step if there is no header)
+    csv_header = next(csvreader)
+    print(f"CSV Header: {csv_header}")
+
+    # Read each row of data after the header
+    for row in csvreader:
+        print(row)
