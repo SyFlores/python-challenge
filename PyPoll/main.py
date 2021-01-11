a18afd3c0f8df1415dc3d7178a771b659bd85cf6
SyFlores <75463000+SyFlores@users.noreply.github.com>
2021-01-10 23:35:14 -0800

    Election Results are now completely calculated (#24)
    * Update Sources, Collaboration, and Thoughts.md
    
    Added notes on looping through a csv multiple times and a source used to convert a list into a dictionary.
    
    * Update main.py
    
    Now calculates the number of votes that each candidate has received and their respective percentage of the total votes.

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -25,5 +25,24 @@ with open(csvpath) as csvfile:
         if row[2] not in cand_list:
             cand_list.append(row[2])
 
+    cand_dict = {cand_list[i]: {"Count" : 0, "Percentage" : 0} for i in range(0,len(cand_list))}
+
+with open(csvpath) as csvfile:
+
+    csvreader = csv.reader(csvfile, delimiter=',')
+
+    print(csvreader)
+
+    # Read the header row first
+    csv_header = next(csvreader)
+
+    for row in csvreader:
+        for candidate in cand_list:
+            if row[2] == candidate:
+                cand_dict[candidate]["Count"] += 1
+
+    for candidate in cand_list:
+        cand_dict[candidate]["Percentage"] = round((cand_dict[candidate]["Count"]*100)/vote_count,3)
+    
 print(str(vote_count))
-print(cand_list)
+print(cand_dict)
