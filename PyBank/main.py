034b9466608b75a355262378738ce63a1cc3d466
SyFlores <75463000+SyFlores@users.noreply.github.com>
2021-01-10 16:03:13 -0800

    Sy flores 3 avg diff (#10)
    * Update main.py
    
    Updated to include mean difference calculation and dictionary created to mimic the csv.
    
    * Delete Sources, Collaboration, and Thoughts.docx
    
    Updating to newer version.
    
    * Add files via upload
    
    Updated to include new sources and thoughts.

--- PyBank/main.py
+++ PyBank/main.py
@@ -22,9 +22,36 @@ with open(csvpath) as csvfile:
     #     print(row)
 
     unique_dates = []
+
+    total_value = 0
+
+    bank_date = []
+    PandL = []
+    PandL_Diff = []
+
+
     for row in csvreader:
+        # Feed unique data values into a list
         if row[0] not in unique_dates:
-            unique_dates.append(row[0]) 
+            unique_dates.append(row[0])
+        
+        # Adding the individual values in the "Profit/Losses" column
+        total_value += int(row[1])
 
+        # Pulling values from the columns into lists
+        bank_date.append(row[0])
+        PandL.append(float(row[1]))
 
+    # Using the above lists to create a dictionary for easy calculations and iteration
+    budget = {"Date": bank_date, "Profit/Losses": PandL}
+
+    # Looping through list to create a difference list
+    for i in range(1, len(budget["Profit/Losses"])):
+        PandL_Diff.append(budget["Profit/Losses"][i] - budget["Profit/Losses"][i-1])
+
+    avg_PandL_Diff = sum(PandL_Diff)/len(PandL_Diff)
+    
+# Output - Data Summary
 print(len(unique_dates))
+print(total_value)
+print(round(avg_PandL_Diff))
